7e7993077b7feb8caf43486d17642e83ad17e4e4
ilyasil2surgut <ilyasil2surgut@gmail.com>
2017-04-05 00:44:11 +0500

    Refined Data and datapoints

--- DataParse.py
+++ DataParse.py
@@ -16,14 +16,19 @@ def FilterOnly(names:list,parsedfile:list)->list:
     return out
 class Datapoint:
     def __init__(self,names:list,line:list):
-        self.data=line
-        self.fields=names
+        self.data=[float(x) for x in line]
+        self.fields=[x.split("/",maxsplit=1)[0] for x in names]
+        self.units=[x.split("/",maxsplit=1)[1] for x in names]
     def get(self,name:str)->float:
-        return self.data[self.fields.index("name")]
-    def pairs(self):
-        return [(self.fields[i],self.data[i]) for i in range(len(self.data))]
-    def __str__(self):
-        return str(self.pairs())
+        return float(self.data[self.fields.index(name)])
+    def set(self,name:str,value:float):
+        self.data[self.fields.index(name)]=value
+    def out(self):
+        return [(self.fields[i],self.data[i],self.units[i]) for i in range(len(self.data))]
+    def __str__(self)->str:
+        return str(["{}:{} {}".format(x[0],x[1],x[2]) for x in self.out()])
+    def Selection(self,firststr:str,secondstr:str)->tuple:
+        return (self.get(firststr),self.get(secondstr))
 class Data:
     datapoints=[]
     def __init__(self,filename:str):
@@ -38,3 +43,14 @@ class Data:
         self.rawdata=FilterOnly(names,self.rawdata)
         self.data = self.rawdata[1:]
         self.fieldnames = self.rawdata[0]
+    def FilterPoints(self,F):
+        newpoints=[]
+        for i,point in enumerate(self.datapoints):
+            if F(point):newpoints.append(point)
+        self.datapoints=newpoints
+    def FixFalseAlt(self,truevalue,falsevalue):
+        for i, point in enumerate(self.datapoints):
+            if point.get("Altitude") != falsevalue:
+                break
+            else:
+                point.set("Altitude", truevalue)
--- Main.py
+++ Main.py
@@ -10,5 +10,9 @@ names = ['Time/s', 'Throttle/%', 'IAS/kph', 'TAS/kph', 'Altitude/m', 'Vario/m/s'
 Spitfire=DataParse.Data("C:/Users/ilyas_000/SkyDrive/Documents/SPITFIRE_IX_4_4_7.33.50 20fuel.csv")
 Spitfire.FilterNames(names)
 Spitfire.GenerateDataPoints()
+Spitfire.FixFalseAlt(184,608)
+maxvalue=max([x.get("Throttle") for x in Spitfire.datapoints])
+Spitfire.FilterPoints((lambda point:point.get('Throttle')==maxvalue))
 for i in range(10):
-    print(Spitfire.datapoints[100+i])
+    print(Spitfire.datapoints[1000+i])
+# print(Spitfire.datapoints[-1])
